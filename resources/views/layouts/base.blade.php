19a7055a53c29e8a2f412307afd2e4a082869027
Putu Krisna Andyartha <43901559+putukrisna6@users.noreply.github.com>
2021-09-05 18:18:55 +0700

    Update base.blade.php

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -8,8 +8,8 @@
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-        <link rel="stylesheet" href="{{ asset('css/app.css', true) }}">
-        <link rel="stylesheet" href="{{ asset('css/styles.css', true) }}">
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+        <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
 
         <!-- Styles -->
         <style>
@@ -60,5 +60,5 @@
             </div>
         </footer>
     </body>
-    <script src="{{ asset('js/app.js', true) }}"></script>
+    <script src="{{ asset('js/app.js') }}"></script>
 </html>
